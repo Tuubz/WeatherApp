e2fe0ca0e7d8cc87e4da5425d10289718155ddaa
Tobias Heuts <tobias@connectica.no>
2018-09-23 01:13:29 +0700

    fragments & adapters

---
+++ app/src/main/java/app/krungsri/weatherapp/view/fragmentadapter/WeatherFragment.kt
@@ -0,0 +1,25 @@
+package app.krungsri.weatherapp.view.fragmentadapter
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.weather.krungsi.weatherapp.R
+
+
+class WeatherFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+
+    companion object {
+        fun newInstance(): WeatherFragment {
+            return WeatherFragment()
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/app/krungsri/weatherapp/view/fragmentadapter/WeatherPagerAdapter.kt
@@ -0,0 +1,26 @@
+package app.krungsri.weatherapp.view.fragmentadapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.PagerAdapter
+
+class WeatherPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0 -> WeatherFragment.newInstance()
+            else -> WeatherForeCastFragment.newInstance()
+        }
+    }
+
+    override fun getItemPosition(item: Any): Int {
+        return PagerAdapter.POSITION_NONE
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+
+}
