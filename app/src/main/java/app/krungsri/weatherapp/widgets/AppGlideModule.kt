7228f8964aef82fe82821d4d328f2e70f5fa1d5b
Tobias Heuts <tobias@connectica.no>
2018-09-24 01:26:27 +0700

    uncessary

--- app/src/main/java/app/krungsri/weatherapp/widgets/AppGlideModule.kt
+++ app/src/main/java/app/krungsri/weatherapp/widgets/AppGlideModule.kt
@@ -1,7 +1,6 @@
 package nimbl3.surveyapp.widgets
 
 import android.content.Context
-import app.weather.krungsi.weatherapp.R
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.GlideBuilder
@@ -13,7 +12,6 @@ class AppGlideModule : AppGlideModule() {
 
     override fun applyOptions(context: Context?, builder: GlideBuilder?) {
         super.applyOptions(context, builder)
-//        val requestOptions = RequestOptions().centerCrop().error(R.drawable.ic_close)
         val requestOptions = RequestOptions().centerCrop()
         builder?.setDefaultRequestOptions(requestOptions)
     }
