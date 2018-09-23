316fd66319cf552427144a3610d535fa10d62f41
Tobias Heuts <tobias@connectica.no>
2018-09-24 00:25:17 +0700

    swithc between celcius & fahrenheit

--- app/src/main/java/app/krungsri/weatherapp/view/activity/WeatherActivity.kt
+++ app/src/main/java/app/krungsri/weatherapp/view/activity/WeatherActivity.kt
@@ -2,7 +2,10 @@ package app.krungsri.weatherapp.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.KeyEvent
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import app.krungsri.weatherapp.view.fragmentadapter.WeatherPagerAdapter
 import app.krungsri.weatherapp.view.fragmentadapter.fragments.WeatherCurrentFragment
@@ -13,8 +16,8 @@ import kotlinx.android.synthetic.main.activity_weather.*
 
 class WeatherActivity : AppCompatActivity() {
 
-
     private lateinit var pagerAdapter: WeatherPagerAdapter
+    private var temperatureType = "metric"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +25,36 @@ class WeatherActivity : AppCompatActivity() {
         init()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.toolbar_right_icon) {
+            if(temperatureType == "metric") {
+                temperatureType = "imperial"
+                item.icon = ContextCompat.getDrawable(applicationContext, R.drawable.ic_fahrenheit)
+            } else {
+                temperatureType = "metric"
+                item.icon = ContextCompat.getDrawable(applicationContext, R.drawable.ic_celcius)
+            }
+            getData(temperatureType)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
     private fun init() {
         initViewPager()
         initToolbar()
-
         setListeners()
     }
 
     private fun initToolbar(){
         setSupportActionBar(toolbar)
-        toolbar.apply {
-//            setNavigationIcon(R.drawable.ic_refresh)
-        }
         supportActionBar?.title = ""
     }
 
@@ -45,16 +66,18 @@ class WeatherActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-
-//        imperial
         cityInput.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
-                (supportFragmentManager.fragments[0] as WeatherCurrentFragment).getData("metric")
-                (supportFragmentManager.fragments[1] as WeatherForeCastFragment).getData("metric")
+                getData(temperatureType)
                 return@OnKeyListener true
             }
             false
         })
+    }
+
+    private fun getData(units: String) {
+        (supportFragmentManager.fragments[0] as WeatherCurrentFragment).getData(units)
+        (supportFragmentManager.fragments[1] as WeatherForeCastFragment).getData(units)
     }
 
 
